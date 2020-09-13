4a28674b3085c9becc58f6266d791fda721a61f9
Guillermo Ortiz <guillermo.ortiz@wizeline.com>
2020-09-12 21:17:38 -0500

    Added meta tags with description

--- pages/_app.js
+++ pages/_app.js
@@ -28,7 +28,16 @@ export default function App({ Component, pageProps }) {
       <Helmet>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="description"
+          content="Fun facts about space and the ISS from NASA."
+        ></meta>
         <title>Astroworld</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>"
+        ></link>
         <style>
           @import
           url('https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap');
