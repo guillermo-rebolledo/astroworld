b28156b2009241dddb9e272090033c9d1721834e
Guillermo Ortiz <guillermo.ortiz@wizeline.com>
2020-09-12 21:39:23 -0500

    remove image

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,6 @@ export default function Home({ astroCount, astroData }) {
         />
         <meta property="twitter:image" content="/astroworld.png" />
       </Head>
-      <img src="/astroworld.png" />
       <Header>
         <span role="img" aria-label="star emoji">
           🌟
